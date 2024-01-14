c0417fd0e8859aaeeb6f39fb176fdc0099ad5bcd
1998rtran <1998rtran@gmail.com>
2024-01-14 04:46:48 -0800

    Repo Set Up

---
+++ BossingDetails.js
@@ -0,0 +1,7 @@
+const BossList = {
+  {
+  'bossname': 'Zakum',
+  'bossdetails': 'Stone Boss',
+  'bossreward': '1,000,000',
+  }
+}
---
+++ client/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import BossingDetails from '/Users/ray/Desktop/Projects/BossingCalculator/BossingDetails.js'
+
+export default function App() {
+  return (
+    <div id="App">
+      <p>Hello, world!</p>
+    </div>
+  );
+}
